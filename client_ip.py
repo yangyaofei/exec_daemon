a61066890fff6187dbac791f1f84dbb11a645887
yangyaofei <yangyaofei@gmail.com>
2016-07-01 12:13:30 +0800

    ip conf to null

--- client_ip.py
+++ client_ip.py
@@ -5,9 +5,10 @@ import logging
 
 logging.basicConfig(level=logging.INFO)
 s = sockets.connectServer()
+ip_c_null = "null"
 ip_c = "192.168.1.3|255.255.255.0|192.168.1.1"
-logging.info(ip_c)
-ip = "[ip]" + ip_c
+logging.info(ip_c_null)
+ip = "[ip]" + ip_c_null
 s.send(ip)
 result = sockets.recv(s)
 logging.info(result)
--- server.py
+++ server.py
@@ -14,6 +14,8 @@ ip_c_str = '''#/bin/sh
 ifconfig eth0 {} netmask {}
 route add default gw {}
 '''
+ip_c_null = "null"
+ip_c_str_null = "#/bin/sh"
 
 
 def dealClient(conn, addr):
@@ -33,18 +35,29 @@ def dealClient(conn, addr):
 	# 修改ip以及其他地址
 	elif szBuf.find(ip_c) == 0:
 		szBuf = szBuf.replace(ip_c, "")
-		argvs = szBuf.split("|")
-		if len(argvs) == 3:
-			text = ip_c_str.format(argvs[0], argvs[1], argvs[2])
-			with open(ip_c_file, "w") as f:
-				f.write(text)
-			out = commands.getstatusoutput("chmod a+x " + ip_c_file)
-			if out[0] == 0:
-				result = "[resp]ip conf ok"
+		# config to auto get ip
+		if '|' not in szBuf:
+			if szBuf.find(ip_c_null) == 0:
+				with open(ip_c_file, "w") as f:
+					f.write(ip_c_str_null)
+				result = "[resp]ip conf null ok"
 			else:
-				result = '[error]ip cong error file i/o error'
+				result = "[error]argvs error"
+		# 设置静态ip
 		else:
-			result = "[error]argvs error"
+			argvs = szBuf.split("|")
+			# 三个参数,否则出错
+			if len(argvs) == 3:
+				text = ip_c_str.format(argvs[0], argvs[1], argvs[2])
+				with open(ip_c_file, "w") as f:
+					f.write(text)
+				out = commands.getstatusoutput("chmod a+x " + ip_c_file)
+				if out[0] == 0:
+					result = "[resp]ip conf ok"
+				else:
+					result = '[error]ip cong error file i/o error'
+			else:
+				result = "[error]argvs error"
 	else:
 		conn.close()
 		return
